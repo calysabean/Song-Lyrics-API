bbd2efb3de6ddd642e9ea56cb2ca97627eece0ac
Courtney Bean <calysabean@gmail.com>
2018-12-02 19:32:20 -0500

    corrected code

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
-function displaySearchData(data) {
+/* This app doesn't follow a11y best practices, and the JS file is incomplete. Complete the getDataFromApi, displaySearchData, and watchSubmit functions. When you're done, this app should allow a user to search for an artist and song title (both should be required), and display the song lyrics. You should make requests to this API: https://lyricsovh.docs.apiary.io/# . Also make any necessary adjustments to make this app accessible. */
+
+function getDataFromApi(artist, title, callback) {
+    //your code here
+  }
+  
+  function displaySearchData(data) {
     //your code here
   }
   
